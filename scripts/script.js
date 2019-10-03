5cdb8538b3a1b5026c1458c8c0b8661e2f07dd0e
apc <alexcurran.va@gmail.com>
2019-10-03 13:49:44 -0500

    Finished with nav layout, working on styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const menuModule = (() => {
-    const aboutSpan = document.querySelector(".nav-list-about-span");
-    const aboutSubMenu = document.querySelector(".nav-list-about");
+// const menuModule = (() => {
+//     const aboutSpan = document.querySelector(".nav-list-about-span");
+//     const aboutSubMenu = document.querySelector(".nav-list-about");
     
-    function toggleSubMenu(event) {
-        aboutSubMenu.classList.toggle("show-sub-menu");
-    }
+//     function toggleSubMenu(event) {
+//         aboutSubMenu.classList.toggle("show-sub-menu");
+//     }
 
-    aboutSpan.addEventListener("click", toggleSubMenu);
-})();
+//     aboutSpan.addEventListener("click", toggleSubMenu);
+
+// })();
