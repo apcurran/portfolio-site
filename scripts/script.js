af267c89388d984f5b81cace9681adb05f66332f
apc <alexcurran.va@gmail.com>
2019-10-03 15:30:17 -0500

    Working on header styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,1 @@
 "use strict";
-
-// const menuModule = (() => {
-//     const aboutSpan = document.querySelector(".nav-list-about-span");
-//     const aboutSubMenu = document.querySelector(".nav-list-about");
-    
-//     function toggleSubMenu(event) {
-//         aboutSubMenu.classList.toggle("show-sub-menu");
-//     }
-
-//     aboutSpan.addEventListener("click", toggleSubMenu);
-
-// })();
