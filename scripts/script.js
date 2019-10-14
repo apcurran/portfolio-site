4dc8433c28f053487c9b766f097d902617bab9e9
apc <alexcurran.va@gmail.com>
2019-10-14 13:13:36 -0500

    Created base HTML and grid layout for about-skills cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,14 @@
 "use strict";
 
-const sectionHighlight = (() => {
-    // const pageSections = document.querySelectorAll(".section");
-
-    // function checkSection(event) {
-    //     for (const section of pageSections) {
-    //         const scrolledFromTop = window.pageYOffset;
-
-    //         const sectionTop = section.getBoundingClientRect().top + scrolledFromTop;
-
-    //         const sectionBottom = sectionTop + section.clientHeight;
-    //         const isShown = scrolledFromTop >= sectionTop;
-    //         const isNotScrolledPast = scrolledFromTop < sectionBottom;
-            
-    //         // Grab link
-    //         const currentPanelData = section.dataset.section;
-    //         const link = document.querySelector(`.${currentPanelData}`);
-
-    //         if (isShown && isNotScrolledPast) {
-    //             link.classList.add("active");
-    //         } else {
-    //             link.classList.remove("active");
-    //         }
-    //     }
-    // }
-
-    // function debounce(func, wait = 20, immediate = true) { // Debounce from Underscore.js
-    //     var timeout;
-    //     return function() {
-    //       var context = this, args = arguments;
-    //       var later = function() {
-    //         timeout = null;
-    //         if (!immediate) func.apply(context, args);
-    //       };
-    //       var callNow = immediate && !timeout;
-    //       clearTimeout(timeout);
-    //       timeout = setTimeout(later, wait);
-    //       if (callNow) func.apply(context, args);
-    //     };
-    // }
-
-    // window.addEventListener("scroll", debounce(checkSection));
-
-    // Intersection Observer API //
-    // Caching DOM els
+const navLinkHighlighter = (() => {
+    // Caching DOM
     const pageSections = document.querySelectorAll(".section");
     const navbarHeight = document.querySelector(".nav").clientHeight;
 
     const options = {
         root: null,
         threshold: 0,
-        rootMargin: `-${navbarHeight}px 0px -65% 0px`
+        rootMargin: `-${navbarHeight}px 0px -60% 0px`
     };
 
     const observerCb = (entries, observer) => {
@@ -77,5 +35,4 @@ const sectionHighlight = (() => {
     for (const section of pageSections) {
         observer.observe(section);
     }
-
 })();
