1d27bbe6526c34eccb29998c0a25ed6f38eab23b
Alex Curran <alexcurran.va@gmail.com>
2021-11-18 11:56:09 -0600

    Remove block scoping and rename const var for form functionality

--- scripts/script.js
+++ scripts/script.js
@@ -9,47 +9,45 @@ function handleNav() {
 
 nav.addEventListener("mouseenter", handleNav, { once: true });
 
-{
-    // Form Validation
-    const form = document.forms.contact;
-    const [name, email, message] = form.elements;
-    const allUserInputs = form.querySelectorAll(".contact-user-input");
+// Form Validation
+const form = document.forms.contact;
+const [username, email, message] = form.elements;
+const allUserInputs = form.querySelectorAll(".contact-user-input");
 
-    function checkValidity(event) {
-        if (event.currentTarget.validity.valid) {
-            const currentEl = event.currentTarget;
-            const error = currentEl.nextElementSibling;
+function checkValidity(event) {
+    if (event.currentTarget.validity.valid) {
+        const currentEl = event.currentTarget;
+        const error = currentEl.nextElementSibling;
 
-            error.textContent = "";
-            error.classList.remove("error-active");
-        }
+        error.textContent = "";
+        error.classList.remove("error-active");
     }
-
-    function finalValidity(event) {
-        allUserInputs.forEach(input => {
-            if (!input.validity.valid || input.value === "") {
-                event.preventDefault();
-                const error = input.nextElementSibling;
-                
-                if (input.name === "name") {
-                    error.textContent = "Please enter your full name.";
-                } else if (input.name === "email") {
-                    error.textContent = "Please enter a valid email address.";
-                } else {
-                    error.textContent = "Please include a message before submitting.";
-                }
-
-                error.classList.add("error-active");
-            }
-        });
-
-    }
-
-    name.addEventListener("blur", checkValidity);
-    email.addEventListener("blur", checkValidity);
-    message.addEventListener("blur", checkValidity);
-    form.addEventListener("submit", finalValidity);
 }
+
+function finalValidity(event) {
+    allUserInputs.forEach(input => {
+        if (!input.validity.valid || input.value === "") {
+            event.preventDefault();
+            const error = input.nextElementSibling;
+            
+            if (input.name === "name") {
+                error.textContent = "Please enter your full name.";
+            } else if (input.name === "email") {
+                error.textContent = "Please enter a valid email address.";
+            } else {
+                error.textContent = "Please include a message before submitting.";
+            }
+
+            error.classList.add("error-active");
+        }
+    });
+
+}
+
+username.addEventListener("blur", checkValidity);
+email.addEventListener("blur", checkValidity);
+message.addEventListener("blur", checkValidity);
+form.addEventListener("submit", finalValidity);
 
 // Copyright year
 const yearSpan = document.querySelector(".copyright-year");
