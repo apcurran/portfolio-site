638db863b7f93c90ef579640f6b54e1130f1e5b6
Alex Curran <alexcurran.va@gmail.com>
2025-07-30 09:52:25 -0500

    Move nav menu logic into separate module JS file

--- src/main.js
+++ src/main.js
@@ -2,39 +2,9 @@
 
 import "./styles/style.scss";
 
-const navSubMenuBtn = document.querySelector(".nav-sub-menu__title");
-const navSubMenu = document.querySelector(".nav-list-about");
+import { initNavMenu } from "./scripts/nav-menu";
 
-function handleNavMenuClick() {
-    if (!navSubMenu.classList.contains("nav-list-about--show")) {
-        openNavMenu();
-    } else {
-        closeNavMenu();
-    }
-}
-
-function handleNavMenuOutsideClick(event) {
-    if (event.target === navSubMenuBtn) return;
-
-    const isOutside = !event.target.closest(".nav-list-about");
-
-    if (isOutside) closeNavMenu();
-}
-
-function openNavMenu() {
-    navSubMenu.classList.add("nav-list-about--show");
-    navSubMenuBtn.classList.add("nav-sub-menu__title--rotate");
-    navSubMenuBtn.ariaExpanded = "true";
-}
-
-function closeNavMenu() {
-    navSubMenu.classList.remove("nav-list-about--show");
-    navSubMenuBtn.classList.remove("nav-sub-menu__title--rotate");
-    navSubMenuBtn.ariaExpanded = "false";
-}
-
-navSubMenuBtn.addEventListener("click", handleNavMenuClick);
-document.addEventListener("click", handleNavMenuOutsideClick);
+initNavMenu();
 
 // Form Validation
 const form = document.forms.contact;
